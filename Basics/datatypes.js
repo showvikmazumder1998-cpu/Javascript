07e49fa1d74b0dea1146defa6d4396085f257c7c
Showvik Mazumder <showvikmazumder1998@gmail.com>
2026-01-16 19:28:21 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,8 @@
+console.log(typeof "Hello");
+console.log(typeof 110n);
+console.log(typeof 33);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
