b0e99fb43a243a22a7f6fa195bfbd09fad4504f5
Showvik Mazumder <showvikmazumder1998@gmail.com>
2026-01-17 10:09:18 +0000

    string operations

---
+++ Basics/string.js
@@ -0,0 +1,12 @@
+const name = "Showvik"
+const city = "Russia"
+
+console.log(`My name is ${name} & I live in ${city}. What are you doing in my repo?`);
+//modern way, mostly used in backend programming.
+let x = new String("Hello")
+console.log(x)
+let y = x.substring(0, 3)
+console.log(y)
+console.log(x.charAt(4));
+console.log(x.indexOf('l'));
+// there are lots of string operations
