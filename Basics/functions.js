83f9ac7da64177803fea239cdd70fef861411d2b
Showvik Mazumder <showvikmazumder1998@gmail.com>
2026-01-25 09:06:37 +0000

    fun functions

---
+++ Basics/functions.js
@@ -0,0 +1,19 @@
+function calPrice(num1) {
+    return num1;
+
+}
+
+const user = {
+    name : "Showvik",
+    age : 23,
+    city : "Cox's Bazar"
+}
+
+function findOut(anyobject){
+    console.log(`This is the name ${anyobject.name} and this is the age ${anyobject.age}`)
+}
+
+console.log(calPrice(2));
+
+findOut(user);
+
